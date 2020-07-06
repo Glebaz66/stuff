29cade9eb2cd0c84270feac1ae4b2e9cfe30f25f
glebaz <glebaz666@gmail.com>
2020-07-06 17:46:31 +0300

    add files

---
+++ goods/db.js
@@ -0,0 +1,8 @@
+const product = [
+    { id: '1', name: 'img/1F.jpg', price: "100" },
+    { id: '2', name: 'img/2F.jpg', price: "200" },
+    { id: '3', name: 'img/3F.jpg', price: "300" },
+    { id: '4', name: 'img/4F.jpg', price: "400" },
+    { id: '5', name: 'img/5F.jpg', price: "500" },
+    { id: '6', name: 'img/6F.jpg', price: "600" },
+];
